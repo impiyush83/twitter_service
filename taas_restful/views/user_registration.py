5f97a6b637b0b054bae431eaa8a0300f6f2f51ad
nalawadepiyush <nalawadepiyush@gmail.com>
2019-02-10 05:04:03 +0530

    Skimmed unrelated files

--- taas_restful/views/user_registration.py
+++
@@ -1,9 +0,0 @@
-from taas_restful.models.user import User
-from taas_restful.utils.custom_exceptions import ResourceAlreadyPresent
-
-
-def user_registration(data):
-    try:
-        User.insert_user(data)
-    except:
-        raise ResourceAlreadyPresent("User already exists with this email address")
