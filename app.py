8caac209571f19d3f527fd41531648ca26c4df05
nalawadepiyush <nalawadepiyush@gmail.com>
2019-03-01 02:49:51 +0530

    Refactored app.py to support gunicorn

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from taas_restful.app import create_app
 from taas_restful.settings import Config
 
-if __name__ == '__main__':
-    app = create_app(Config)
-    app.run()
+
+app = create_app(Config)
