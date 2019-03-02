c89498651dbffc5012247fa02c39edaa42894ae1
nalawadepiyush <nalawadepiyush@gmail.com>
2019-03-02 20:32:59 +0530

    Refactored for heroku config

--- taas_restful/models/db.py
+++ taas_restful/models/db.py
@@ -1,9 +1,11 @@
-from flask import current_app as app, Config
+import os
+
+from flask import Config
 from sqlalchemy_wrapper import SQLAlchemy
 
 config_name = 'taas_restful.settings.Config'
 config = Config("")
 config.from_object(config_name)
 
-db = SQLAlchemy(uri=config['DATABASE_URL'])
+db = SQLAlchemy(uri=os.environ.get('DATABASE_URL'))
 Model = db.Model
